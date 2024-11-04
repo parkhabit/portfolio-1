39b91df1fb3df4e9a1a0693746ced1d39879a710
Kitty <parkhabit@gmail.com>
2024-11-04 15:25:11 +1100

    adds underlying grey background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="bg-[#FFD6E3] flex flex-1 flex-col p-3">
-      <header className="flex flex-row justify-between">
+      <header className="flex flex-row justify-between sticky top-0 p-1">
         <p className="text-sm">
           Kitty Allen. Frontend Software Engineer, Sydney.
         </p>
@@ -23,6 +23,18 @@ export default function Home() {
 
         <div className="grid grid-cols-3 gap-4 relative mt-2">
           <div className="bg-[#FFD6E380] absolute w-full h-full" />
+
+          <div className="flex flex-1 bg-slate-500 h-80"></div>
+          <div className="flex flex-1 bg-slate-500"></div>
+          <div className="flex flex-1 bg-slate-500"></div>
+
+          <div className="flex flex-1 bg-slate-500 h-80"></div>
+          <div className="flex flex-1 bg-slate-500"></div>
+          <div className="flex flex-1 bg-slate-500"></div>
+
+          <div className="flex flex-1 bg-slate-500 h-80"></div>
+          <div className="flex flex-1 bg-slate-500"></div>
+          <div className="flex flex-1 bg-slate-500"></div>
 
           <div className="flex flex-1 bg-slate-500 h-80"></div>
           <div className="flex flex-1 bg-slate-500"></div>
