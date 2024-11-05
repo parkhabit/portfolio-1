132b4c554ef6391279a386d8dc2877ce6b623152
Kitty <parkhabit@gmail.com>
2024-11-05 16:50:56 +1100

    refactoring

--- src/components/DynamicHeading.tsx
+++ src/components/DynamicHeading.tsx
@@ -1,19 +1,19 @@
 export default function DynamicHeading() {
   return (
-    <div className="flex flex-col">
-      <p className="text-sm flex">
-        Kitty Allen.
-        <span className="font-mono overflow-hidden whitespace-nowrap inline-block relative animate-typewriter1 after:content-['|'] after:absolute after:right-0 after:animate-caret">
-          Frontend Software Engineer
-        </span>
-        <span className="font-mono overflow-hidden whitespace-nowrap inline-block relative animate-typewriter2 after:content-['|'] after:absolute after:right-0 after:animate-caret">
-          Accessibility advocate
-        </span>
-        <span className="font-mono overflow-hidden whitespace-nowrap inline-block relative animate-typewriter3 after:content-['|'] after:absolute after:right-0 after:animate-caret">
-          Design systems enthusiast
-        </span>
-      </p>
-      <p className="text-sm">Sydney.</p>
+    <div className="flex flex-row items-center">
+      <p className="text-sm pr-1">Kitty Allen.</p>
+
+      <span className="font-mono text-sm overflow-hidden whitespace-nowrap inline-block relative animate-typewriter1 after:content-['|'] after:absolute after:right-0 after:animate-caret">
+        Frontend Software Engineer
+      </span>
+      <span className="font-mono text-sm overflow-hidden whitespace-nowrap inline-block relative animate-typewriter2 after:content-['|'] after:absolute after:right-0 after:animate-caret">
+        Accessibility advocate
+      </span>
+      <span className="font-mono text-sm overflow-hidden whitespace-nowrap inline-block relative animate-typewriter3 after:content-['|'] after:absolute after:right-0 after:animate-caret">
+        Design systems enthusiast
+      </span>
+
+      <p className="text-sm pl-1">Sydney.</p>
     </div>
   );
 }
